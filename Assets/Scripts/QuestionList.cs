a2492299cc85a3c270e82cce43a6365964bb3755
mikeyr013 <mericha4@asu.edu>
2022-03-09 17:57:46 -0700

    Changed drag and drop to lock into place, adjusted number of answers and data format, and refactored

--- Assets/Scripts/QuestionList.cs
+++ Assets/Scripts/QuestionList.cs
@@ -5,13 +5,113 @@ using System;
 
 public class AnswerData
 {
-    public static string answerData = "{\r\n  \"questions\": [\r\n    {\r\n      \"question\": \"Psyche is the name of?\",\r\n      \"correct_answer\": \"An asteroid\",\r\n      \"answers\": [\r\n        \"An asteroid\", \"A person\", \"A department\", \"A telescope brand\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"What are most asteroids made of?\",\r\n      \"correct_answer\": \"Rock or ice\",\r\n      \"answers\": [\r\n        \"Rock or ice\", \"Metal\", \"Chocolate\", \"Sand\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"When is the launch of the Psyche spacecraft from Cape Canaveral, Florida?\",\r\n      \"correct_answer\": \"2022\",\r\n      \"answers\": [\r\n        \"2022\", \"2023\", \"2024\", \"It already launched\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"Who has seen the Psyche asteroid?\",\r\n      \"correct_answer\": \"No one\",\r\n      \"answers\": [\r\n        \"No one\", \"Some astronauts\", \"Some scientists\", \"Some teachers\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"Does the Psyche asteroid have gravity?\",\r\n      \"correct_answer\": \"Yes\",\r\n      \"answers\": [\r\n        \"Yes\", \"No\", \"Sometimes\", \"Only if you carry a magnet\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"How long with the spacecraft fly?\",\r\n      \"correct_answer\": \"3.4 years\",\r\n      \"answers\": [\r\n        \"3.4 years\", \"3.4 months\", \"3.4 days\", \"3.4 hours\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"What is the size of the spacecraft?\",\r\n      \"correct_answer\": \"Roughly the size of one tennis court\",\r\n      \"answers\": [\r\n        \"Roughly the size of one tennis court\", \"Roughly the size of one football field\", \"Roughly the size of a basketball\", \"Roughly the size of a farm\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"What are the terrestrial planets?\",\r\n      \"correct_answer\": \"Mercury, Venus, Earth and Mars\",\r\n      \"answers\": [\r\n        \"Mercury, Venus, Earth and Mars\", \"Mercury, Jupiter, Earth and Mars\", \"Saturn, Venus, Earth and Mars\", \"Mercury, Venus, Earth and Neptune\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"When will the spacecraft flyby Mars?\",\r\n      \"correct_answer\": \"2023\",\r\n      \"answers\": [\r\n        \"2023\", \"2022\", \"2024\", \"2030\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"How many phases does the Psyche mission have?\",\r\n      \"correct_answer\": \"four\",\r\n      \"answers\": [\r\n        \"four\", \"three\", \"five\", \"one\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"When did we discover what Psyche is made of?\",\r\n      \"correct_answer\": \"2010\",\r\n      \"answers\": [\r\n        \"2010\", \"2021\", \"1990\", \"2000\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"When was the Psyche mission selected by NASA?\",\r\n      \"correct_answer\": \"Jan 4, 2017\",\r\n      \"answers\": [\r\n        \"Jan 4, 2017\", \"Mar 4, 2013\", \"Apr 4, 2019\", \"Dec 4, 2021\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"What company built the spacecraft?\",\r\n      \"correct_answer\": \"Maxar Technologies\",\r\n      \"answers\": [\r\n        \"Maxar Technologies\", \"Blue Origin\", \"SpaceX\", \"Sierra Nevada Corporation\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"What will happen at the end of the mission?\",\r\n      \"correct_answer\": \"The spacecraft will stay in orbit around Psyche\",\r\n      \"answers\": [\r\n        \"The spacecraft will stay in orbit around Psyche\", \"The spacecraft will return to Earth\", \"The spacecraft will go to another asteroid\", \"The spacecraft will go to a black hole\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"What is Psyche orbiting?\",\r\n      \"correct_answer\": \"The Sun\",\r\n      \"answers\": [\r\n        \"The Sun\", \"The moon\", \"Jupiter\", \"Mars\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"Who is the mission led by?\",\r\n      \"correct_answer\": \"Arizona State University\",\r\n      \"answers\": [\r\n        \"Arizona State University\", \"University of Arizona\", \"Stanford University\", \"NASA\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"How many times has NASA investigated a planet made of metal before?\",\r\n      \"correct_answer\": \"Never\",\r\n      \"answers\": [\r\n        \"Never\", \"Once\", \"Twice\", \"Four Times\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"What is being sent to Psyche to investigate?\",\r\n      \"correct_answer\": \"Spacecraft\",\r\n      \"answers\": [\r\n        \"Spacecraft\", \"Spaceship\", \"Astronaut\", \"Alien\"\r\n        ]\r\n    },\r\n    {\r\n      \"question\": \"Who discovered the Psyche asteroid?\",\r\n      \"correct_answer\": \"Annibale de Gasparis\",\r\n      \"answers\": [\r\n        \"Annibale de Gasparis\", \"George Washington\", \"Abraham Lincoln\", \"Bill Gates\"\r\n        ]\r\n    }\r\n    ]\r\n}";
+    public static string answerData =
+    @"{
+       ""questions"": [
+            {
+              ""question"": ""Psyche is the name of?"",
+              ""correct_answer"": ""An asteroid"",
+              ""wrong_answer"": ""A person""
+            },
+            {
+              ""question"": ""What are most asteroids made of?"",
+              ""correct_answer"": ""Rock or ice"",
+              ""wrong_answer"": ""Metal""
+            },
+            {
+              ""question"": ""When is the launch of the Psyche spacecraft from Cape Canaveral, Florida?"",
+              ""correct_answer"": ""2022"",
+              ""wrong_answer"": ""2021""
+            },
+            {
+              ""question"": ""Who has been to the Psyche asteroid?"",
+              ""correct_answer"": ""No one"",
+              ""wrong_answer"": ""Some astronauts""
+            },
+            {
+              ""question"": ""Does the Psyche asteroid have gravity?"",
+              ""correct_answer"": ""Yes"",
+              ""wrong_answer"": ""No""
+            },
+            {
+              ""question"": ""How long with the spacecraft fly?"",
+              ""correct_answer"": ""3.4 years"",
+              ""wrong_answer"": ""3.4 hours""
+            },
+            {
+              ""question"": ""What is the size of the spacecraft?"",
+              ""correct_answer"": ""Roughly the size of one tennis court"",
+              ""wrong_answer"": ""Roughly the size of one football field""
+            },
+            {
+              ""question"": ""What are the terrestrial planets?"",
+              ""correct_answer"": ""Mercury, Venus, Earth and Mars"",
+              ""wrong_answer"": ""Mercury, Jupiter, Earth and Mars""
+            },
+            {
+              ""question"": ""When will the spacecraft flyby Mars?"",
+              ""correct_answer"": ""2023"",
+              ""wrong_answer"": ""2024""
+            },
+            {
+              ""question"": ""How many phases does the Psyche mission have?"",
+              ""correct_answer"": ""Four"",
+              ""wrong_answer"": ""Five""
+            },
+            {
+              ""question"": ""When did we discover what Psyche is made of?"",
+              ""correct_answer"": ""2010"",
+              ""wrong_answer"": ""1990""
+            },
+            {
+              ""question"": ""When was the Psyche mission selected by NASA?"",
+              ""correct_answer"": ""Jan 4, 2017"",
+              ""wrong_answer"": ""Mar 4, 2013""
+            },
+            {
+              ""question"": ""What company built the spacecraft?"",
+              ""correct_answer"": ""Maxar Technologies"",
+              ""wrong_answer"": ""SpaceX""
+            },
+            {
+              ""question"": ""What will happen at the end of the mission?"",
+              ""correct_answer"": ""The spacecraft will stay in orbit around Psyche"",
+              ""wrong_answer"": ""The spacecraft will return to Earth""
+            },
+            {
+              ""question"": ""What is Psyche orbiting?"",
+              ""correct_answer"": ""The Sun"",
+              ""wrong_answer"": ""Jupiter""
+            },
+            {
+              ""question"": ""Who is the mission led by?"",
+              ""correct_answer"": ""Arizona State University"",
+              ""wrong_answer"": ""University of Arizona""
+            },
+            {
+              ""question"": ""How many times has NASA investigated a planet made of metal before?"",
+              ""correct_answer"": ""Never"",
+              ""wrong_answer"": ""Twice""
+            },
+            {
+              ""question"": ""What is being sent to Psyche to investigate?"",
+              ""correct_answer"": ""Spacecraft"",
+              ""wrong_answer"": ""Astronaut""
+            },
+            {
+              ""question"": ""Who discovered the Psyche asteroid?"",
+              ""correct_answer"": ""Annibale de Gasparis"",
+              ""wrong_answer"": ""Galileo Galilei""
+            }
+        ]
+    }";
+
 }
 
 [Serializable]
 public class QuestionList
 {
-    public Question[] questions;
+    public List<Question> questions;
 }
 
 [Serializable]
@@ -19,5 +119,5 @@ public class Question
 {
     public string question;
     public string correct_answer;
-    public string[] answers;
+    public string wrong_answer;
 }
