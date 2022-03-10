089414f29f0d4781750480257884a2b8bdc4e6c3
Gwaegs <70778891+Gwaegs@users.noreply.github.com>
2022-03-09 19:52:40 -0700

    Update QuestionList.cs

--- Assets/Scripts/QuestionList.cs
+++ Assets/Scripts/QuestionList.cs
@@ -16,17 +16,17 @@ public class AnswerData
             {
               ""question"": ""What are most asteroids made of?"",
               ""correct_answer"": ""Rock or ice"",
-              ""wrong_answer"": ""Metal""
+              ""wrong_answer"": ""Chocolate""
             },
             {
               ""question"": ""When is the launch of the Psyche spacecraft from Cape Canaveral, Florida?"",
-              ""correct_answer"": ""2022"",
-              ""wrong_answer"": ""2021""
+              ""correct_answer"": ""launch is in 2022"",
+              ""wrong_answer"": """"
             },
             {
               ""question"": ""Who has been to the Psyche asteroid?"",
               ""correct_answer"": ""No one"",
-              ""wrong_answer"": ""Some astronauts""
+              ""wrong_answer"": """"
             },
             {
               ""question"": ""Does the Psyche asteroid have gravity?"",
@@ -36,67 +36,67 @@ public class AnswerData
             {
               ""question"": ""How long will the Psyche Mission last?"",
               ""correct_answer"": ""3.4 years"",
-              ""wrong_answer"": ""3.4 hours""
+              ""wrong_answer"": ""3.4 seconds""
             },
             {
               ""question"": ""What is the size of the spacecraft?"",
               ""correct_answer"": ""The size of one tennis court"",
-              ""wrong_answer"": ""The size of one football field""
+              ""wrong_answer"": """"
             },
             {
               ""question"": ""What are the terrestrial planets?"",
               ""correct_answer"": ""Mercury, Venus, Earth and Mars"",
-              ""wrong_answer"": ""Mercury, Jupiter, Earth and Mars""
+              ""wrong_answer"": """"
             },
             {
               ""question"": ""When will the Psyche spacecraft fly by Mars?"",
-              ""correct_answer"": ""2023"",
-              ""wrong_answer"": ""2024""
+              ""correct_answer"": ""it will fly by in 2023"",
+              ""wrong_answer"": """"
             },
             {
               ""question"": ""How many phases are there for the Psyche mission?"",
               ""correct_answer"": ""Four"",
-              ""wrong_answer"": ""Five""
+              ""wrong_answer"": ""Three""
             },
             {
               ""question"": ""How long have we known what Psyche is made of?"",
               ""correct_answer"": ""Since 2010"",
-              ""wrong_answer"": ""Since 1990""
+              ""wrong_answer"": """"
             },
             {
               ""question"": ""When was the Psyche mission selected by NASA?"",
               ""correct_answer"": ""Jan 4, 2017"",
-              ""wrong_answer"": ""Mar 4, 2013""
+              ""wrong_answer"": """"
             },
             {
               ""question"": ""What company built the spacecraft?"",
               ""correct_answer"": ""Maxar Technologies"",
-              ""wrong_answer"": ""SpaceX""
+              ""wrong_answer"": """"
             },
             {
               ""question"": ""What will happen at the end of the mission?"",
               ""correct_answer"": ""The spacecraft will stay in orbit around Psyche"",
-              ""wrong_answer"": ""The spacecraft will return to Earth""
+              ""wrong_answer"": """"
             },
             {
               ""question"": ""What is the Psyche asteroid orbiting?"",
               ""correct_answer"": ""The Sun"",
-              ""wrong_answer"": ""Jupiter""
+              ""wrong_answer"": """"
             },
             {
               ""question"": ""Who is the mission led by?"",
               ""correct_answer"": ""Arizona State University"",
-              ""wrong_answer"": ""University of Arizona""
+              ""wrong_answer"": """"
             },
             {
-              ""question"": ""What is being sent to Psyche to investigate?"",
-              ""correct_answer"": ""Spacecraft"",
-              ""wrong_answer"": ""Astronaut""
+              ""question"": ""Is Earth made of metal like Psyche?"",
+              ""correct_answer"": ""Np"",
+              ""wrong_answer"": ""Yes""
             },
             {
               ""question"": ""Who discovered the Psyche asteroid?"",
               ""correct_answer"": ""Annibale de Gasparis"",
-              ""wrong_answer"": ""Galileo Galilei""
+              ""wrong_answer"": """"
             }
         ]
     }";
